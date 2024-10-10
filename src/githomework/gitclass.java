79fe43225b5c5ae9503dcea0cd8dba040d9ab86c
Mayuritester <computer.tester009@gmail.com>
2024-10-10 16:08:25 +0100

    git projoect

---
+++ src/githomework/gitclass.java
@@ -0,0 +1,13 @@
+package githomework;
+
+public class gitclass {
+
+public static void main(String[] args) {
+
+    int x = 5;
+            int y = 6;
+
+            System.out.println(x+y);
+
+}
+}
